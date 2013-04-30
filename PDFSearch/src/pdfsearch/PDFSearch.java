1b4a757bdfb1e9cce410b0585556be94bd6362cd
Lars Rasmussen <lars@1337.dk>
2013-04-30 13:04:36 +0200

    Add folder iteration for PDF discovery

--- PDFSearch/src/pdfsearch/PDFSearch.java
+++ PDFSearch/src/pdfsearch/PDFSearch.java
@@ -5,9 +5,11 @@
 package pdfsearch;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongField;
@@ -35,13 +37,19 @@ public class PDFSearch {
 	public static void main(String[] args) {
 		PDFSearch s = new PDFSearch(new MMapIndexFactory());
 		
-		long start = System.currentTimeMillis();
+		/*long start = System.currentTimeMillis();
 		s.addPDFs();
 		long end = System.currentTimeMillis();
 		System.out.println(end-start + " ms");
 		s.search("javascript");
 		long end2 = System.currentTimeMillis();
-		System.out.println(end2-end + " ms");
+		System.out.println(end2-end + " ms");*/
+		
+		File dir = new File("C:\\Users\\Lars\\Documents\\GitHub\\synchronize-pdfsearch\\PDFs");
+		
+		ArrayList<File> files = s.getFiles(dir);
+		for(File f : files)
+			System.out.println(f.getPath());
 	}
 	
 	public void search(String term){
@@ -55,6 +63,53 @@ public class PDFSearch {
 			System.err.println("ParseException in search");
 			System.err.println(e);
 		}
+	}
+	
+	public ArrayList<File> getFiles(String dir){
+		File start = new File(dir);
+		return getFiles(start);
+	}
+	
+	FileFilter pdfAndDirectoryFileFilter = new FileFilter(){
+		@Override
+		public boolean accept(File file) {
+			// If this is a directory, we have a valid type
+			if(file.isDirectory())
+				return true;
+
+			// Check whether we have a PDF file
+			byte[] pdfMagicNumbers = {0x25, 0x50, 0x44, 0x46};
+			try(FileInputStream ins = new FileInputStream(file)){
+				for(int i = 0; i < pdfMagicNumbers.length; i++){
+					if(ins.read() != pdfMagicNumbers[i])
+						return false;
+				}
+			}catch(IOException e){
+				return false;
+			}
+			return true;
+		}
+	};
+	
+	public ArrayList<File> getFiles(File start){		
+		if(!start.isDirectory())
+			return new ArrayList<File>();
+		
+		// Create an arraylist containing all PDF files
+		ArrayList<File> pdfs = new ArrayList<File>();
+		
+		// Get the files and directories in our current filter
+		File[] files = start.listFiles(pdfAndDirectoryFileFilter);
+		
+		// Iterate through all files/folders and add files to our PDF ArrayList, and recurse on subfolders
+		for(File f : files){
+			if(f.isDirectory())
+				pdfs.addAll(getFiles(f));
+			else
+				pdfs.add(f);
+		}
+		
+		return pdfs;
 	}
 	
 	public void addPDFs(){
